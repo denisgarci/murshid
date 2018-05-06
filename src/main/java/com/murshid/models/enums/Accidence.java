c0eb47b9db65ffe3f54a41f873cc79561d4d0ec7
gonzalodiaz <gonzalo.diaz@radius.com>
2018-05-06 11:49:47 -0700

    add accidence category

--- src/main/java/com/murshid/models/enums/Accidence.java
+++ src/main/java/com/murshid/models/enums/Accidence.java
@@ -1,22 +1,26 @@
 package com.murshid.models.enums;
 
+import static com.murshid.models.enums.AccidenceCategory.*;
+
 public enum Accidence {
-    MASCULINE("masculine"), FEMININE("feminine"), NEUTER("neuter"),
-    PLURAL("plural"), SINGULAR("singular"),
-    LONG_FORM("long form"),
-    INDICATIVE("indicative"), SUBJUNCTIVE("subjunctive"), CONDITIONAL("conditional"), IMPERATIVE("imperative"), PRESUMPTIVE("presumptive"),
-    FACTUAL("factual"), CONTRAFACTUAL("contrafactual"),
-    _1ST("first person"), _2ND("second person"), _3RD("thrid person"),
-    ACTIVE("active"), PASSIVE("passive"),
-    NON_ASPECTUAL("non aspectual"), HABITUAL("habitual"), CONTINUOUS("continuous"), PERFECTIVE("perfective"), IMPERFECTIVE("imperfective"),
-    PRESENT("present"), FUTURE("future"), PAST("past"), PLUSQUAMPERFECT("plusquamperfect"),
-    DIRECT("direct"), OBLIQUE("oblique"), VOCATIVE("vocative");
+    MASCULINE("masculine", GENDER), FEMININE("feminine", GENDER), NEUTER("neuter", GENDER),
+    PLURAL("plural", NUMBER), SINGULAR("singular", NUMBER),
+    LONG_FORM("long form", FORM_LENGTH),
+    INDICATIVE("indicative", MODE), SUBJUNCTIVE("subjunctive", MODE), CONDITIONAL("conditional", MODE), IMPERATIVE("imperative", MODE), PRESUMPTIVE("presumptive", MODE),
+    FACTUAL("factual", FACTUALITY), CONTRAFACTUAL("contrafactual", FACTUALITY),
+    _1ST("first person", PERSON), _2ND("second person", PERSON), _3RD("thrid person", PERSON),
+    ACTIVE("active", VOICE), PASSIVE("passive", VOICE),
+    NON_ASPECTUAL("non aspectual", ASPECT), HABITUAL("habitual", ASPECT), CONTINUOUS("continuous", ASPECT), PERFECTIVE("perfective", ASPECT), IMPERFECTIVE("imperfective", ASPECT),
+    PRESENT("present", TENSE), FUTURE("future", TENSE), PAST("past", TENSE), PLUSQUAMPERFECT("plusquamperfect", TENSE),
+    DIRECT("direct", CASE), OBLIQUE("oblique", CASE), VOCATIVE("vocative", CASE);
 
 
     private String label;
+    private AccidenceCategory accidenceCategory;
 
-    Accidence(String label){
+    Accidence(String label, AccidenceCategory accidenceCategory){
         this.label = label;
+        this.accidenceCategory = accidenceCategory;
     }
 
     public static Accidence fromLabel(String label){
@@ -27,5 +31,11 @@ public enum Accidence {
         }
         throw new IllegalArgumentException("label " + label + " not supported");
     }
+
+    public AccidenceCategory getAccidenceCategory() {
+        return accidenceCategory;
+    }
+
+
 
 }
---
+++ src/main/java/com/murshid/models/enums/AccidenceCategory.java
@@ -0,0 +1,5 @@
+package com.murshid.models.enums;
+
+public enum AccidenceCategory {
+    GENDER, CASE, NUMBER, PERSON,  TENSE, VOICE, MODE, ASPECT, FACTUALITY, FORM_LENGTH
+}
